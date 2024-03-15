45f242b749c5a8b3c057bebc87dce27793a33089
mariana-santos <marianasfernandessousa@gmail.com>
2024-03-14 21:39:47 -0300

    feat: inicia segurança com spring

--- src/main/java/br/com/fiap/CashFlow/controllers/UsuarioController.java
+++ src/main/java/br/com/fiap/CashFlow/controllers/UsuarioController.java
@@ -1,73 +1,26 @@
 package br.com.fiap.CashFlow.controllers;
 
-import java.util.List;
-
-import jakarta.validation.Valid;
-
-import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.server.ResponseStatusException;
-
-import br.com.fiap.CashFlow.model.Usuario;
+import br.com.fiap.CashFlow.model.dto.Token;
 import br.com.fiap.CashFlow.repository.UsuarioRepository;
+import br.com.fiap.CashFlow.service.TokenService;
 
 @RestController
 @RequestMapping("usuarios")
-@Slf4j
 public class UsuarioController {
 
     @Autowired
     UsuarioRepository usuarioRepository;
 
-    @GetMapping
-    public List<Usuario> listAll() {
-        log.info("Buscando Todos os Usuarios");
-        return usuarioRepository.findAll();
-    }
+    @Autowired
+    TokenService service;
 
-    @GetMapping("{id}")
-    public ResponseEntity<Usuario> readUsuario(@PathVariable Long id) {
-        log.info("Exibindo o Usuario de ID: " + id);
-        return ResponseEntity.ok(getUsuarioById(id));
-    }
-
-    @PostMapping
-    public ResponseEntity<Usuario> createUsuario(@RequestBody @Valid Usuario novo_usuario) {
-        log.info("Cadastrando Usuario: " + novo_usuario);
-        usuarioRepository.save(novo_usuario);
-        return ResponseEntity.status(HttpStatus.CREATED).body(novo_usuario);
-    }
-
-    @PutMapping("{id}")
-    public ResponseEntity<Usuario> updateUsuario(@PathVariable @Valid Long id,
-            @RequestBody Usuario usuario_atualizar) {
-        log.info("Atualizando o Usuario de ID: " + id);
-        getUsuarioById(id);
-        usuario_atualizar.setId(id);
-        usuarioRepository.save(usuario_atualizar);
-        return ResponseEntity.ok(usuario_atualizar);
-    }
-
-    @DeleteMapping("{id}")
-    public ResponseEntity<Usuario> deleteUsuario(@PathVariable Long id) {
-        log.info("Deletando o Usuario de ID: " + id);
-        usuarioRepository.delete(getUsuarioById(id));
-        return ResponseEntity.noContent().build();
-    }
-
-    private Usuario getUsuarioById(Long id) {
-        return usuarioRepository.findById(id).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario não encontrado com o ID: " + id));
+    @PostMapping("/login")
+    public ResponseEntity<Token> login(){
+        return ResponseEntity.ok( service.generateToken("mari@fiap.com.br") );
     }
 }
---
+++ src/main/java/br/com/fiap/CashFlow/model/dto/Token.java
@@ -0,0 +1,7 @@
+package br.com.fiap.CashFlow.model.dto;
+
+public record Token(
+    String token,
+    String type,
+    String prefix
+) {} 
